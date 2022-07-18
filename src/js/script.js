a99b62feeea142d76ae2ca41fd4b77ffb21e1d74
Michał Bielecki <mb9246@gmail.com>
2022-07-18 14:30:33 +0200

    zad 3 wstepnie

--- src/js/script.js
+++ src/js/script.js
@@ -31,38 +31,48 @@ function render() { // nowa funkcja render
     //Wygenerowany element DOM dołącz jako nowe dziecko DOM do listy .books-list.
     thisBookList.appendChild(generatedDOM);
   }   
-};
+}
 
 /* 
 2. Dodaj do JS-a pustą tablicę "favoriteBooks" oraz odpowiedni kod, który zadba o to, aby przy dwukliku na okładkę dowolnej książki, jej id było dodawane do tej tablicy, a do samego elementu HTML została dodana klasa favorite. 
+
+3. Zmodyfikuj swoją funkcję przypiętą do nasłuchiwacza, aby najpierw sprawdzała, czy książka nie jest już w "ulubionych". Jeśli nie jest, to funkcja ma dodać klasę favorite i zapisać id książki w tablicy favoriteBooks. Jeśli jednak jest, to skrypt powinien usuwać id takiej książki z tablicy favoriteBooks oraz zabierać takiemu elementowi klasę favorite.
 */
 
-// Dodanie nowej tablicy 
+// Dodanie nowej tablicy (2)
 const favouriteBooks = [];
 
-// Utowrzenie nowej funkcji "initActions"
+// Utowrzenie nowej funkcji "initActions" (2)
 function initActions(){
 
-  // referencję do listy wszystkich elementów .book__image w liście .booksList.
+  // referencję do listy wszystkich elementów .book__image w liście .booksList. (2)
   const allElems = document.querySelectorAll(select.containerOf.image);
 
-  // przejdź po każdym elemencie z tej listy.
+  // przejdź po każdym elemencie z tej listy. (2)
   for(let elem of allElems){
 
-    // Dla każdego z nich dodaj nasłuchiwacz, który po wykryciu uruchomi funkcję, która...
+    // Dla każdego z nich dodaj nasłuchiwacz, który po wykryciu uruchomi funkcję, która... (2)
     elem.addEventListener('dbclick', function(event){
 
-      // ...zatrzyma domyślne zachowanie przeglądarki
+      // ...zatrzyma domyślne zachowanie przeglądarki (2)
       event.preventDefault();
     });
 
-    // doda do klikniętego elementu klasę favorite,
-    clickedElement.classlist.add('favorite');
+    // Sprawdź, czy element zawiera klasę .favorite. Jeśli ma, to również oznacza to, że tak dana książka musi być już w "ulubionych". (3)
+    if(clickedElement.classlist.contains('favorire')){
+        
+      // jeśli tak, to usunie z kilkniętego elementu klase favorite (3)
+      clickedElement.classlist.remove('favorite');
+    } else {
 
-    // pobierze z jego data-id identyfikator książki,
+      // doda do klikniętego elementu klasę favorite, (2)
+      clickedElement.classlist.add('favorite');
+    }
+    
+    // pobierze z jego data-id identyfikator książki, (2)
     const bookId = clickedElement.getAttribute('data-id');
 
-    // i doda ten identyfikator do favoriteBooks
+    // i doda ten identyfikator do favoriteBooks (2)
     favouriteBooks.push(bookId);
   }
 }
